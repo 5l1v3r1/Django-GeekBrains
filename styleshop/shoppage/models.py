451a21f9a30e3cacfe87a12f5c51b47bd3086f80
DimasGon <DimasGon@users.noreply.github.com>
2018-07-24 16:35:05 +0300

    Добавление цены и брэнда к модели Product

--- styleshop/shoppage/models.py
+++ styleshop/shoppage/models.py
@@ -4,8 +4,10 @@ class Product(models.Model):
 
     name = models.CharField(max_length=20)
     category = models.ForeignKey('mainpage.Category', on_delete=models.CASCADE)
+    brand = models.ForeignKey('mainpage.Brand', on_delete=models.CASCADE)
     sex = models.ForeignKey('mainpage.Sex', on_delete=models.CASCADE)
     images = models.ImageField(upload_to='products')
+    cost = models.DecimalField(max_digits=7, decimal_places=2)
 
     def __str__(self):
         return ' - '.join([self.name, str(self.category)])
--- styleshop/shoppage/views.py
+++ styleshop/shoppage/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from . import models
+from mainpage.models import Section, Category, Brand
 
 def shop(request):
 
     products = models.Product.objects.all()
+    sections = Section.objects.all()
+    categories = Category.objects.all()
+    brands = Brand.objects.all()
 
-    return render(request, 'shoppage/shop.html', {'products': products})
+    return render(
+        request, 'shoppage/shop.html', 
+        {
+            'products': products,
+            'sections': sections,
+            'categories': categories,
+            'brands': brands,
+        }
+    )
 
 def single_product_details(request):
 
