5dc7938a0071ac01f663ff18264dde9e13a036a7
DimasGon <DimasGon@users.noreply.github.com>
2018-07-24 11:06:47 +0300

    Создание и регистрации модели Product приложения shoppage

--- styleshop/shoppage/models.py
+++ styleshop/shoppage/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+
+    name = models.CharField(max_length=20)
+    section = models.ForeignKey('mainpage.Section', on_delete=models.CASCADE)
+    category = models.ForeignKey('mainpage.Category', on_delete=models.CASCADE)
+    sex = models.ForeignKey('mainpage.Sex', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return ' - '.join([self.name, str(self.category)])
