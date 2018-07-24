a599560b6093eef66c479469ea795f1b6297a357
DimasGon <DimasGon@users.noreply.github.com>
2018-07-24 11:50:39 +0300

    Добавление поля image в Product

--- styleshop/shoppage/models.py
+++ styleshop/shoppage/models.py
@@ -6,6 +6,7 @@ class Product(models.Model):
     section = models.ForeignKey('mainpage.Section', on_delete=models.CASCADE)
     category = models.ForeignKey('mainpage.Category', on_delete=models.CASCADE)
     sex = models.ForeignKey('mainpage.Sex', on_delete=models.CASCADE)
+    images = models.ImageField(upload_to='product/{}'.format(name))
 
     def __str__(self):
         return ' - '.join([self.name, str(self.category)])
