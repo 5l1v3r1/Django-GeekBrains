9f3590af5c08d3de475388ff6ce303d67dc4f067
DimasGon <DimasGon@users.noreply.github.com>
2018-08-08 16:58:00 +0300

    Изменения в джанго-админ панели Products и Brands

--- styleshop/mainpage/admin.py
+++ styleshop/mainpage/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
+from django.template.loader import render_to_string
 from . import models
 
+@admin.register(models.Brand)
+class BrandAdmin(admin.ModelAdmin):
+    
+    list_display = ('name', 'picture')
+
+    def picture(self, obj):
+
+        return render_to_string('mainpage/admin/picture.html', {'image': obj.image})
+
 admin.site.register(models.Section)
-admin.site.register(models.Brand)
 admin.site.register(models.Sex)
 admin.site.register(models.Category)
--- styleshop/shoppage/admin.py
+++ styleshop/shoppage/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Product)
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'category', 'brand', 'cost', 'sale')
+    list_filter = ('category', 'brand')
+
+    search_fields = ('name',)
+
+    fieldsets = (
+        ('Information', {
+            'fields': ('name', ('category', 'brand', 'sex'),)
+        }),
+        ('Content', {
+            'fields': (('cost', 'sale'), 'images', 'description')
+        })
+    )
