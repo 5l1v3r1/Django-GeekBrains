39be8c5ef6b953733d3858a700c0168762fdb28a
DimasGon <DimasGon@users.noreply.github.com>
2018-08-12 16:40:16 +0300

    Создание форм для категорий, брэндов, секций и пола

--- styleshop/adminapp/forms.py
+++ styleshop/adminapp/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from shoppage.models import Product
+from mainpage.models import Sex, Brand, Section, Category
 
 class ProductForm(forms.ModelForm):
 
@@ -29,4 +30,74 @@ class ProductForm(forms.ModelForm):
             'sale': 'Размер скидки',
             'images': 'Фотография',
             'description': 'О товаре'
+        }
+
+class BrandForm(forms.ModelForm):
+
+    class Meta:
+
+        model = Brand
+
+        fields = ['name', 'image']
+
+        labels = {
+            'name': 'Название', 
+            'image':'Логотип'
+        }
+
+        widgets = {
+            'name': forms.widgets.TextInput(attrs={'class': 'input100'}),
+            'image': forms.widgets.ClearableFileInput()
+        }
+
+class SexForm(forms.ModelForm):
+
+    class Meta:
+
+        model = Sex
+
+        fields = ['name']
+
+        labels = {
+            'name': 'Название', 
+        }
+
+        widgets = {
+            'name': forms.widgets.TextInput(attrs={'class': 'input100'}),
+        }
+
+class SectionForm(forms.ModelForm):
+
+    class Meta:
+
+        model = Section
+
+        fields = ['name']
+
+        labels = {
+            'name': 'Название', 
+        }
+
+        widgets = {
+            'name': forms.widgets.TextInput(attrs={'class': 'input100'}),
+        }
+
+class CategoryForm(forms.ModelForm):
+
+    class Meta:
+
+        model = Category
+
+        fields = ['name', 'section', 'sex']
+
+        labels = {
+            'name': 'Название', 
+            'section': 'Секция', 
+            'sex': 'Пол'
+        }
+
+        widgets = {
+            'name': forms.widgets.TextInput(attrs={'class': 'input100'}),
+            'section': forms.widgets.Select(attrs={'class': 'input100', 'style': 'padding-left: 5px'}),
+            'sex': forms.widgets.Select(attrs={'class': 'input100', 'style': 'padding-left: 5px'}),
         }
