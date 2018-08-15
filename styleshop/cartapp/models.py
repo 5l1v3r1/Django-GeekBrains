272aaf188803ce42416c09490abcf94f50cf1a3c
DimasGon <DimasGon@users.noreply.github.com>
2018-08-15 18:45:15 +0300

    Создание модели доставки

--- styleshop/cartapp/models.py
+++ styleshop/cartapp/models.py
@@ -20,3 +20,12 @@ class Cart(models.Model):
             return amount
 
         return None
+
+class Delivery(models.Model):
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING)
+    cart = models.ForeignKey(Cart, on_delete=models.DO_NOTHING)
+    country = models.CharField(max_length=30, required=True)
+    city = models.CharField(max_length=30, required=True)
+    address = models.CharField(max_lenght=60, required=True)
+    terms_and_conitions = models.BooleanField(default=False)
