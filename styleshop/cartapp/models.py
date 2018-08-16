53cf64024631ec3355724d2da3d151fca42ae1bf
DimasGon <DimasGon@users.noreply.github.com>
2018-08-16 17:48:28 +0300

    Revert "Разработка внесения нескольких товаров в одну корзину"
    This reverts commit a58bb422d4c6bf791a8c93bbcf5e763bd6a22dc7.

--- styleshop/cartapp/models.py
+++ styleshop/cartapp/models.py
@@ -7,6 +7,7 @@ class Cart(models.Model):
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     products = lib.DictField()
+    # product = models.ForeignKey(Product, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
 
     @staticmethod
@@ -14,8 +15,9 @@ class Cart(models.Model):
         
         if not request.user.is_anonymous:
             
-            cart = Cart.objects.get(user=request.user)
-            amount = sum([cart.products[obj]['quantity'] for obj in cart.products if cart.products])
+            cart = Cart.objects.filter(user=request.user)
+            # amount = sum(list(map(lambda obj: obj.quantity, cart)))
+            amount = sum([obj.quantity for obj in cart])
             return amount
 
         return None
